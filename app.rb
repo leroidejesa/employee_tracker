68ae2ca3604df89a3fc5ca61e4191f9586ff037c
leroi & axel <student@epicodus.com>
2015-05-11 16:46:01 -0700

    Added HR manager page where one can view and create divisions

--- app.rb
+++ app.rb
@@ -16,6 +16,23 @@ get('/hr_manager') do
   erb(:hr_manager)
 end
 
+get('/divisions/new') do
+  erb(:division_form)
+end
+
+post('/divisions') do
+  name = params.fetch("name")
+  division = Division.new({:name => name, :id => nil})
+  division.save()
+  erb(:success)
+end
+
+get('/divisions/:id') do
+  @division = Division.find(params.fetch("id").to_i())
+  @employees = @division.employees()
+  erb(:division)
+end
+
 get('/project_manager') do
   @projects = Project.all()
   erb(:project_manager)
